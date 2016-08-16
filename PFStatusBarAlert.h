97decee0d9ec3d4248c75e79bcbfd67d3947d478
Bailey Seymour <bailey@baileyseymour.com>
2016-08-16 13:54:37 -0400

    compiler erros

--- PFStatusBarAlert.h
+++ PFStatusBarAlert.h
@@ -6,8 +6,8 @@
 @property (nonatomic, copy, readonly) NSString *notification; // notification to listen for (prefs changed) NOTE: optional
 @property (nonatomic, retain) UIColor *backgroundColor; // modify if you want a different color than default
 @property (nonatomic, retain) UIColor *textColor; // modify if you want a different color than default
-@property (nonatomic, assign, readonly) SEL action; // action to send when tapped
-@property (nonatomic, retain, readonly) id target; // target to send action to when tapped
+@property (nonatomic, assign) SEL action; // action to send when tapped
+@property (nonatomic, retain) id target; // target to send action to when tapped
 @property (nonatomic, retain) UIWindow *statusBarOverlay; // overlay window
 @property (nonatomic, retain) UIButton *actionButton; // main text button
 - (void)showOverlayForSeconds:(float)seconds; // this will be called for 5.5s automatically on notification. NOTE: if no notification is set call manually
